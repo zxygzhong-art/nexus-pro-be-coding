a3fd1471a1b3a6d8060d7d692242cea9373b51f5
yannis.zhong.gx <zxygzhong@gauxon.com>
2026-06-23 14:44:18 +0800

    refactor: group domain alias exports

--- internal/repository/memory/aliases.go
+++ internal/repository/memory/aliases.go
@@ -2,36 +2,42 @@ package memory
 
 import "nexus-pro-be/internal/domain"
 
-type Tenant = domain.Tenant
-type Account = domain.Account
-type UserGroup = domain.UserGroup
-type PermissionSet = domain.PermissionSet
-type Permission = domain.Permission
-type PermissionSetAssignment = domain.PermissionSetAssignment
-type DataScope = domain.DataScope
-type FieldPolicy = domain.FieldPolicy
-type AssumableRole = domain.AssumableRole
-type AssumableRoleSession = domain.AssumableRoleSession
-type OrgUnit = domain.OrgUnit
-type Employee = domain.Employee
-type EmployeeExperience = domain.EmployeeExperience
-type EmployeeQuery = domain.EmployeeQuery
-type EmployeeImportSession = domain.EmployeeImportSession
-type EmployeeImportRow = domain.EmployeeImportRow
-type LeaveBalance = domain.LeaveBalance
-type LeaveRequest = domain.LeaveRequest
-type FormTemplate = domain.FormTemplate
-type FormInstance = domain.FormInstance
-type KnowledgeArticle = domain.KnowledgeArticle
-type Reference = domain.Reference
-type AgentRun = domain.AgentRun
-type CheckResult = domain.CheckResult
-type RowError = domain.RowError
-type AuditLog = domain.AuditLog
-type AuthzOutboxEvent = domain.AuthzOutboxEvent
-type AuthzRelationshipTuple = domain.AuthzRelationshipTuple
-type PageRequest = domain.PageRequest
+// Domain aliases keep the in-memory store concise while preserving exact domain types.
+type (
+	Tenant                  = domain.Tenant
+	Account                 = domain.Account
+	UserGroup               = domain.UserGroup
+	PermissionSet           = domain.PermissionSet
+	Permission              = domain.Permission
+	PermissionSetAssignment = domain.PermissionSetAssignment
+	DataScope               = domain.DataScope
+	FieldPolicy             = domain.FieldPolicy
+	AssumableRole           = domain.AssumableRole
+	AssumableRoleSession    = domain.AssumableRoleSession
+	OrgUnit                 = domain.OrgUnit
+	Employee                = domain.Employee
+	EmployeeExperience      = domain.EmployeeExperience
+	EmployeeQuery           = domain.EmployeeQuery
+	EmployeeImportSession   = domain.EmployeeImportSession
+	EmployeeImportRow       = domain.EmployeeImportRow
+	LeaveBalance            = domain.LeaveBalance
+	LeaveRequest            = domain.LeaveRequest
+	FormTemplate            = domain.FormTemplate
+	FormInstance            = domain.FormInstance
+	KnowledgeArticle        = domain.KnowledgeArticle
+	Reference               = domain.Reference
+	AgentRun                = domain.AgentRun
+	CheckResult             = domain.CheckResult
+	RowError                = domain.RowError
+	AuditLog                = domain.AuditLog
+	AuthzOutboxEvent        = domain.AuthzOutboxEvent
+	AuthzRelationshipTuple  = domain.AuthzRelationshipTuple
+	PageRequest             = domain.PageRequest
+)
 
-const DefaultPage = domain.DefaultPage
-const DefaultPageSize = domain.DefaultPageSize
-const MaxPageSize = domain.MaxPageSize
+// Pagination aliases mirror the domain defaults used by in-memory list helpers.
+const (
+	DefaultPage     = domain.DefaultPage
+	DefaultPageSize = domain.DefaultPageSize
+	MaxPageSize     = domain.MaxPageSize
+)
